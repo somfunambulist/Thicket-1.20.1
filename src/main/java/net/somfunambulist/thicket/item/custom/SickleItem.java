7ac130454bf7c30b0277583fa5aa8a7dbbdd6339
somfunambulist <mccoymylesd@gmail.com>
2025-02-10 17:36:10 -0500

    added gold sickle

--- src/main/java/net/somfunambulist/thicket/item/custom/SickleItem.java
+++ src/main/java/net/somfunambulist/thicket/item/custom/SickleItem.java
@@ -3,11 +3,6 @@ package net.somfunambulist.thicket.item.custom;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.ShearsItem;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.somfunambulist.thicket.item.ModItemProperties;
-
-import java.util.logging.Level;
 
 public class SickleItem extends ShearsItem {
 
