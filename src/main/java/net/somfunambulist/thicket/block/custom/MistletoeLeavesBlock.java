891440aa3da438d0e0ca7faa56dcb880aac63ffd
somfunambulist <mccoymylesd@gmail.com>
2025-02-23 19:09:45 -0500

    fixed mistletoe tick rate

--- src/main/java/net/somfunambulist/thicket/block/custom/MistletoeLeavesBlock.java
+++ src/main/java/net/somfunambulist/thicket/block/custom/MistletoeLeavesBlock.java
@@ -26,31 +26,32 @@ public class MistletoeLeavesBlock extends ModLeavesBlock{
         }
 
         if (!pLevel.isAreaLoaded(pPos, 3)) return; // Forge: prevent loading unloaded chunks when checking neighbor's light and spreading
+        if (pRandom.nextInt(500) == 0) {
+            BlockState blockstate = this.defaultBlockState();
+            for (int i = 0; i < 4; ++i) {
+                int j = 24;
 
-        BlockState blockstate = this.defaultBlockState();
-        for(int i = 0; i < 4; ++i) {
-            int j = 24;
-
-            for(BlockPos blockpos0 : BlockPos.betweenClosed(pPos.offset(-4, -4, -4), pPos.offset(4, 4, 4))) {
-                if (pLevel.getBlockState(blockpos0).is(ModBlockTags.MISTLETOE_BLOCK)) {
-                    --j;
-                    if (j <= 0) {
-                        return;
+                for (BlockPos blockpos0 : BlockPos.betweenClosed(pPos.offset(-4, -4, -4), pPos.offset(4, 4, 4))) {
+                    if (pLevel.getBlockState(blockpos0).is(ModBlockTags.MISTLETOE_BLOCK)) {
+                        --j;
+                        if (j <= 0) {
+                            return;
+                        }
                     }
                 }
-            }
-            BlockPos blockpos = pPos.offset(pRandom.nextInt(3) - 1, pRandom.nextInt(5) - 3, pRandom.nextInt(3) - 1);
-            if (pLevel.getBlockState(blockpos).is(BlockTags.LEAVES) && !pLevel.getBlockState(blockpos).is(ModBlockTags.MISTLETOE_BLOCK)) {
-                var bstate = pLevel.getBlockState(blockpos);
-                if (pRandom.nextInt(3) == 0) {
-                    blockstate = ModBlocks.BLOOMING_MISTLETOE_LEAVES.get().defaultBlockState();
-                    pLevel.setBlockAndUpdate(blockpos, blockstate.setValue(PERSISTENT, bstate.getValue(PERSISTENT)).setValue(WATERLOGGED, bstate.getValue(WATERLOGGED)).setValue(DISTANCE, bstate.getValue(DISTANCE)));
-                } else {
-                    blockstate = ModBlocks.MISTLETOE_LEAVES.get().defaultBlockState();
-                    pLevel.setBlockAndUpdate(blockpos, blockstate.setValue(PERSISTENT, bstate.getValue(PERSISTENT)).setValue(WATERLOGGED, bstate.getValue(WATERLOGGED)).setValue(DISTANCE, bstate.getValue(DISTANCE)));
+                BlockPos blockpos = pPos.offset(pRandom.nextInt(3) - 1, pRandom.nextInt(5) - 3, pRandom.nextInt(3) - 1);
+                if (pLevel.getBlockState(blockpos).is(BlockTags.LEAVES) && !pLevel.getBlockState(blockpos).is(ModBlockTags.MISTLETOE_BLOCK)) {
+                    var bstate = pLevel.getBlockState(blockpos);
+                    if (pRandom.nextInt(3) == 0) {
+                        blockstate = ModBlocks.BLOOMING_MISTLETOE_LEAVES.get().defaultBlockState();
+                        pLevel.setBlockAndUpdate(blockpos, blockstate.setValue(PERSISTENT, bstate.getValue(PERSISTENT)).setValue(WATERLOGGED, bstate.getValue(WATERLOGGED)).setValue(DISTANCE, bstate.getValue(DISTANCE)));
+                    } else {
+                        blockstate = ModBlocks.MISTLETOE_LEAVES.get().defaultBlockState();
+                        pLevel.setBlockAndUpdate(blockpos, blockstate.setValue(PERSISTENT, bstate.getValue(PERSISTENT)).setValue(WATERLOGGED, bstate.getValue(WATERLOGGED)).setValue(DISTANCE, bstate.getValue(DISTANCE)));
+                    }
+                    //pState.setValue(PERSISTENT, false);
+                    //pState.setValue(DISTANCE, bstate.getValue(DISTANCE));
                 }
-                //pState.setValue(PERSISTENT, false);
-                //pState.setValue(DISTANCE, bstate.getValue(DISTANCE));
             }
         }
     }
